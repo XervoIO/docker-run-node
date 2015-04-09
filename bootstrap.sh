af0587f1eaa07f08522495cb049b558963990357
Brandon Cannaday <brandon@modulus.io>
2015-04-09 02:01:26 -0400

    Fix for nvm.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,14 @@ set -x
 export HOME=/root
 export TEMP_DIR=/root/tmp
 
+# TODO: add /opt/modulus/bin to /etc/environment
+
 # Install nvm
 mkdir -p /opt/nvm
 export NVM_DIR=/opt/nvm
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | bash
+
+# TODO: rethink this
 chown mop:mop /opt/nvm/nvm.sh
 chmod g-w /opt/nvm/nvm.sh
 
